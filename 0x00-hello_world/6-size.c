dcb76ad1d38e777923fc4909c3baea5a13ac199c
rennycode9 <rennycode9@gmail.com>
2022-12-08 16:32:34 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+
+*main - Entry point
+
+*Description - returns the puts function
+
+*Return: Always 0 (Success)
+
+*/
+int main(void)
+{
+char c;
+int i;
+long int n;
+long long int  ln;
+float f;
+
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld  byte(s)\n", sizeof(n));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(ln));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
+return (0);
+}
