0eb677ae64e281b9bd75556b84978ec70ebe1899
rennycode9 <rennycode9@gmail.com>
2022-12-08 17:21:59 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@ int main(void)
 {
 char c;
 int i;
-long int li;
-long long int ln;
+long int a;
+long long int b;
 float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu  byte(s)\n", (unsigned long)sizeof(li));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ln));
+printf("Size of a long int: %lu  byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
