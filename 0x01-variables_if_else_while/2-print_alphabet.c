069f05af8c7d7cfb970af8a9361676a78fa8bd5f
rennycode9 <rennycode9@gmail.com>
2022-12-09 17:46:03 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+**main- Entry Point
+*Return: Always 0
+*/
+int main(void)
+{
+char a = 'a'
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+putchar('/n');
+}
+return (0);
+}
