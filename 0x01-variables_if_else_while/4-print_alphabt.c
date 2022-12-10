04de9e4e3210bf2137711e84538a9feb73ec8e0b
rennycode9 <rennycode9@gmail.com>
2022-12-09 18:31:54 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main- Entry Point
+*Return: Always 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a != 'q' && a != 'e')
+{
+putchar(a);
+};
+};
+putchar('\n');
+return (0);
+};
