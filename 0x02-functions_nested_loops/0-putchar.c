35acc9489e816e1a566188385178c7b9073dc5c5
rennycode9 <rennycode9@gmail.com>
2022-12-13 11:10:39 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  *main - entry point
  *Return: Always 0
 */
 int main(void)
 {
-char name[] = "_putchar";
-
-printf("%s\n", name);
+_putchar();
 return (0);
+}
+
+/**
+ *_putchar - Prints "_putchar"
+*/
+void _putchar(void)
+{
+char name[] = "_putchar";
+printf("%s\n", name);
+return;
 }
