9245b1ce93a5c7991ab5a841b1987250ad07931f
rennycode9 <rennycode9@gmail.com>
2022-12-13 15:54:57 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
+#include "main.h"
 /**
  *main - Check Code
  *Return: Always 0.
 */
-
-void print_alphabet(void);
 
 int main(void)
 {
@@ -20,7 +19,23 @@ void print_alphabet(void)
 {
 for (int i = 'a'; i <= 'z'; i++)
 {
-putchar(i);
+_putchar(i);
 }
-putchar('\n');
+_putchar('\n');
+}
+
+#include <unistd.h>
+
+
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
 }
