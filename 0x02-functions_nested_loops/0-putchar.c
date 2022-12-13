6950243498990b2344d58a7c6f3f51211ed4e7f8
rennycode9 <rennycode9@gmail.com>
2022-12-13 15:09:54 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,37 @@
 #include <unistd.h>
-#include "main.h"
-/**
- *main - entry point
- *Description - It prints the word "_putcchar", followed by a new line
- *Return: Always 0
-*/
-int main(void)
-{
-  char word[8] = "_putchar";
-  int i;
 
-  for (i = 0; i < 8; i++)
-    {
+#include "main.h"
+
+
+
+/**
+
+ * main - Check description
+
+ * Description: It prints the word Holberton, followed by a new line.
+
+ * Return: 0.
+
+ */
+
+int main(void)
+  
+{
+  
+  char word[9] = "Holberton";
+  
+  int i;
+  
+
+  
+  for (i = 0; i < 9; i++)
+    
     _putchar(word[i]);
-	}
+  
   _putchar('\n');
+  
+
+  
   return (0);
+  
 }
