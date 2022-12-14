64b196e1d97c92449218360a15f64f47a4a78b3c
rennycode9 <rennycode9@gmail.com>
2022-12-14 07:29:24 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,16 +6,15 @@
 */
 int print_last_digit(int r)
 {
-  int n;
-  if (r < 0)
-    {
-      n = -1 * (r % 10);
-    }
-  else
-    {
-      n = r % 10;
-    }
-  
-  _putchar(n + '0');
-  return (n);
+int n;
+if (r < 0)
+{
+n = -1 * (r % 10);
+}
+else
+{
+n = r % 10;
+}
+_putchar(n + '0');
+return (n);
 }
