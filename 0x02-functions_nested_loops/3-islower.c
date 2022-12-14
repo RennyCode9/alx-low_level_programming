efb6ccc98128bb915e6c2bb2f13009b04be41b6f
rennycode9 <rennycode9@gmail.com>
2022-12-14 05:05:09 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - Check main.h
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int main(int a)
+{
+char i;
+int lower c;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == a)
+{
+lower = 1;
+}
+}
+return lower;
+}
