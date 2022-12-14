0d173c17f35e53854586ccbedabff80eba62d8d3
rennycode9 <rennycode9@gmail.com>
2022-12-13 22:42:40 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
  *main - Check the Code
  *Return: Always 0
 */
 
-void print_alphabet_x10(void);
-
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
-
-/**
- *print_alphabet_x10 - Prints Alphabets 10 times
-*/
 void print_alphabet_x10(void)
 {
-for (int r = '0'; r <= '9'; r++)
+char r,a;
+for (r = '0'; r <= '9'; r++)
 {
-for (int a = 'a'; a <= 'z'; a++)
-{
-putchar(a);
+for (a = 'a'; a <= 'z'; a++)
+{	  
+_putchar(a);
 }
-putchar('\n');
-}
+_putchar('\n');
+}   
 }
