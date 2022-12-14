1bc0e1113cd5f6965bddb8cefb06525cf7431eaf
rennycode9 <rennycode9@gmail.com>
2022-12-13 22:22:25 -0800

    Task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,35 +7,10 @@
 
 int main(void)
 {
-print_alphabet();
+  for (int i = 'a'; i <= 'z'; i++)
+    {
+      _putchar(i);
+    }
+  _putchar('\n');
 return (0);
-}
-
-/**
- *print_alphabet - Prints alphabets
-*/
-
-void print_alphabet(void)
-{
-for (int i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-}
-
-#include <unistd.h>
-
-
-
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
 }
